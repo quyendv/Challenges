a0e02a4cf736d1a4f4346c19d8d31edd0031f0ef
quyendv <daovanquyen45@gmail.com>
2021-11-17 10:34:41 +0700

    windowSliding + LongestSubtringWithoutReapingCharacters

--- src/LeetCode/LinkedList/ListNode.java
+++ src/LeetCode/LinkedList/ListNode.java
@@ -3,6 +3,9 @@ package LeetCode.LinkedList;
 public class ListNode {
     int val;
     ListNode next;
+
+    ListNode() {};
+
     ListNode(int val) {
         this.val = val;
         this.next = null;
