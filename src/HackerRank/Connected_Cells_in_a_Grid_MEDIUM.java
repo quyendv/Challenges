0f0a75206b4556f3ffba12826708e2a04258b439
quyendv <daovanquyen45@gmail.com>
2021-11-27 23:05:49 +0700

    DFS + BFS week12 DSA

--- src/HackerRank/Connected_Cells_in_a_Grid_MEDIUM.java
+++ src/HackerRank/Connected_Cells_in_a_Grid_MEDIUM.java
@@ -9,22 +9,16 @@ có giá trị 1 nhưng visited = false) tức dfs hết cả vùng liên thông
 duyệt hết cả vùng đó)
  + trong hàm main ta duyệt toàn mảng grid đầu vào, nếu phần tử nào = 1 và !visited thì tính số phần tử vùng liên thông chứa số đó.
 Tiếp tục tìm phần tử số 1 nào đó ở VÙNG KHÁC(vì vùng trước sẽ được duyệt hết rồi) và so sánh các vùng rồi in ra max
-
-
 ** Khung solution tham khảo của cô Châu:
 Tóm tắt: Cho mảng hai chiều với các phần tử (ô) có giá trị là 0 hoặc 1. Hai phần từ được gọi là kết nối (connected) với nhau nếu
 chúng cùng có giá trị là 1 và ở sát nhau theo chiều ngang, chiều dọc và chéo cạnh. Như thế mỗi ô có thể có nhiều nhất 8 ô ở kề nối với nó.
-
 Các ô kết nối với nhau tạo thành vùng. Hãy tìm và in ra số phần tử của vùng lớn nhất.
-
 Gợi ý: Xem đây là một đồ thị với đỉnh là các ô, và hai cạnh nối với nhau nếu 2 ô là kết nối (connected). Với mỗi đỉnh (ô), dùng thuật
 toán duyệt theo chiều sau (DFS) hoặc duyệt theo chiều rộng (BFS) để lan ra hết các đỉnh (ô) liên thông với nó. Khi duyệt, nếu đỉnh nào
 đã thăm rồi thì không duyệt nữa. Nói cách khác, đây là bài toán tìm thành phần liên thông của đồ thị.
-
 Mã nguồn: Sinh viên có thể sử dụng phần chương trình sau: https://drive.google.com/file/d/0B7mScmczCiHWRGUwWFBITmFNdWc/view?resourcekey=0-9skcsa6_uslMsQ3meZHyvg
         <bài bên dưới làm thêm từ link mẫu>
 trong đó viết sẵn việc nhập dữ liệu, khai báo mảng các ô grid[][] cũng như mảng lưu trạng thái các ô đã được thăm visited[][] chưa.
-
 Sinh viên cần viết nốt phần hàm static int count_connected(int row, int col) đếm số ô của thành phần liên thông với ô có toạ độ (row, col). Trong hàm này cũng cần cập nhật/đánh dấu các ô đã thăm để không duyệt nữa vào mảng visited[][] được khai báo ở ngoài hàm.
  */
 
